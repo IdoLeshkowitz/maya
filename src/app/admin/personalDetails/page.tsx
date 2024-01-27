5f1b30f36cf5bcfe011814b91200e2ca0a458199
Ido Leshkowitz <idoleshkowitz@gmail.com>
2024-01-27 19:54:11 +0200

    personal details admin

--- src/app/admin/personalDetails/page.tsx
+++ src/app/admin/personalDetails/page.tsx
@@ -5,15 +5,31 @@ async function getAllUserDetails() {
     return prisma.userDetails.findMany()
 }
 
-export default async function PersonalDetailsPage() {
-    const userDetails = await getAllUserDetails()
+async function getAllSessions() {
+    return prisma.experimentSession.findMany()
+}
 
+export default async function PersonalDetailsPage() {
+    const userDetails = await getAllUserDetails() ?? []
+    const sessions = await getAllSessions() ?? []
+    const experimentVersions = Array.from(new Set(sessions.map(session => session.experimentVersion)))
+    console.log(JSON.stringify(sessions.filter(session => session.experimentVersion === "1.0.0")))
     return (
         <div className="min-h-screen bg-white min-w-max pt-5">
             <div className="mb-5">
-                <CsvLink data={userDetails || []} fileName="user-details.csv">
-                    Download CSV
-                </CsvLink>
+                {
+                    experimentVersions.map((version, index) => {
+                        return (
+                            <CsvLink key={index} data={JSON.stringify(sessions.filter(session => session.experimentVersion === version))} fileName={`sessions-${version}.csv`}>
+                                Download CSV for {version}
+                            </CsvLink>
+                        )
+                    }
+                    )
+                }
+                {/*<CsvLink data={JSON.stringify(userDetails)} fileName="user-details.csv">*/}
+                {/*    Download CSV*/}
+                {/*</CsvLink>*/}
             </div>
         </div>
     )
