89929287a6cf57b73c013700614b45f48a636f29
Ido Leshkowitz <idoleshkowitz@gmail.com>
2024-01-27 21:09:27 +0200

    seperated user details answers

--- src/app/admin/personalDetails/page.tsx
+++ src/app/admin/personalDetails/page.tsx
@@ -13,6 +13,21 @@ export default async function PersonalDetailsPage() {
     const userDetails = await getAllUserDetails() ?? []
     const sessions = await getAllSessions() ?? []
     const experimentVersions = Array.from(new Set(sessions.map(session => session.experimentVersion)))
+    const prolificIdByExperimentVersion = experimentVersions.reduce((acc, version) => {
+        acc[version] = sessions.filter(session => session.experimentVersion === version).map(session => session.prolificId)
+        return acc
+    }, {} as Record<string, string[]>)
+    const userDetailsByProlificId = experimentVersions.reduce((acc, version) => {
+        acc[version] = userDetails.filter(userDetail => prolificIdByExperimentVersion[version].includes(userDetail.ProlificId)).map(userDetail => {
+            const parsedAnswers = JSON.parse(userDetail.answers ?? {} as any)
+            return {
+                ...userDetail,
+                ...parsedAnswers
+            }
+        })
+        return acc
+    }, {} as Record<string, any[]>)
+
     return (
         <div className="min-h-screen bg-white min-w-max pt-5">
             <div className="mb-5">
