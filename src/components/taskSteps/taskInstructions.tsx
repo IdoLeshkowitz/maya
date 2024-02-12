4cb3a9f53b65b8e00b38e4815c1b43fa0af74365
Ido Leshkowitz <idoleshkowitz@gmail.com>
2024-02-12 22:01:35 +0200

    merging areen phrasing with main

--- src/components/taskSteps/taskInstructions.tsx
+++ src/components/taskSteps/taskInstructions.tsx
@@ -19,20 +19,20 @@ export default function TaskInstructions(props: TaskInstructionsProps) {
             taskId={props.taskId}
             key={2}
         />,
-        `For each company stock, you will see: \n(1) the portfolio that invested in this stock \n(2) the stock's industry \n(3) whether the stock's value rose or fell over the last week. \nThis is how it would look like if the value of a company's stock from a certain portfolio and industry rose over the last week.`,
+        `For each company stock, you will see: \n(1) the portfolio that invested in this stock \n(2) the stock's industry \n(3)  By how much a stock's value rose over the last week. \nThis is how it would look like if the value of a company's stock from a certain portfolio and industry rose by 450$ over the last week.`,
         <Board
             key={4}
             taskId={props.taskId}
-            snapshot={{indicator: 'check', groupIndex: 0, optionSide: 'RIGHT', label: 'company 1'}}
+            snapshot={{indicator: 450, groupIndex: 0, optionSide: 'RIGHT', label: 'company 1'}}
             header={<Header centered={true}>Stock 1</Header>}
         />,
-        "And this is how it would look if the value of the company's stock from a certain portfolio and industry fell.",
-        <Board
-            key={6}
-            taskId={props.taskId}
-            snapshot={{indicator: 'cross', groupIndex: 0, optionSide: 'RIGHT', label: 'company 2'}}
-            header={<Header centered={true}>Stock 2</Header>}
-        />,
+        //"And this is how it would look if the value of the company's stock from a certain portfolio and industry fell.",
+        // <Board
+        //   key={6}
+        //   taskId={props.taskId}
+        //   snapshot={{indicator: 'cross', groupIndex: 0, optionSide: 'RIGHT', label: 'company 2'}}
+        //   header={<Header centered={true}>Stock 2</Header>}
+        ///>,
         `The performance of all the stocks in the portfolios will be presented one after the other. Please pay close attention. After the presentation, you will be asked questions about the portfolios. \n When you are ready, please start the task.`
     ]
 
